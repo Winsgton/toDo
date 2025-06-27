366f02804600cb8257153a7d248c64b813afe88e
Winsgton <Winsgton>
2025-06-27 17:17:33 -0300

    feat: sidenav | style: navbar
    * criação do sidenav
    * ajuste do navbar com estilização e logout

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from '../../core/services/auth.service';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+            MatToolbarModule, MatButtonModule, MatIconModule,
+            MatSidenavModule, MatListModule
+  ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
+
 export class MainLayoutComponent {
+
+  menuAberto = false;
+
+  constructor(private authService: AuthService, private router: Router){}
+
+  logout(): void {    
+    console.log('fui acionado');
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
